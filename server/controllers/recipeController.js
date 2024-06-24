58dc466148bc74f2aa116d36b7a40e0cab33e70e
Simran Saini <91106038+1simransaini@users.noreply.github.com>
2024-06-24 21:05:52 +0530

    Update recipeController.js

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -181,43 +181,35 @@ exports.submitRecipe = async(req,res)=>{
 */
 exports.submitRecipeOnPost = async(req, res) => {
     try {
-  
-      let imageUploadFile;
-      let uploadPath;
-      let newImageName;
-  
-      if(!req.files || Object.keys(req.files).length === 0){
-        console.log('No Files where uploaded.');
-      } else {
-  
-        imageUploadFile = req.files.image;
-        newImageName = Date.now() + imageUploadFile.name;
-  
-        uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
-  
-        imageUploadFile.mv(uploadPath, function(err){
-          if(err) return res.status(500).send(err);
-        })
-  
-      }
-  
-      const newRecipe = new Recipe({
-        name: req.body.name,
-        description: req.body.description,
-        email: req.body.email,
-        ingredients: req.body.ingredients,
-        category: req.body.category,
-        image: newImageName
-      });
-      
-      await newRecipe.save();
-  
-      req.flash('infoSubmit', 'Recipe has been added.')
-      res.redirect('/submit-recipe');
+        let imageUploadFile = req.files.image;
+        let newImageName = Date.now() + imageUploadFile.name;
+        let uploadPath = require('path').join(__dirname, '/public/uploads/', newImageName);
+
+        imageUploadFile.mv(uploadPath, function (err) {
+            if (err) {
+                console.error('Error uploading file:', err);
+                req.flash('infoErrors', err);
+                return res.redirect('/submit-recipe'); // Redirect on error
+            }
+        });
+
+        const newRecipe = new Recipe({
+            name: req.body.name,
+            description: req.body.description,
+            email: req.body.email,
+            ingredients: req.body.ingredients,
+            category: req.body.category,
+            image: newImageName
+        });
+
+        await newRecipe.save();
+
+        req.flash('infoSubmit', 'Recipe has been added.');
+        res.redirect('/submit-recipe');
     } catch (error) {
-      // res.json(error);
-      req.flash('infoErrors', error);
-      res.redirect('/submit-recipe');
+        console.error('Error saving recipe:', error);
+        req.flash('infoErrors', error);
+        res.redirect('/submit-recipe'); // Redirect on error
     }
   }
 
